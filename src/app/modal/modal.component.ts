fbd6e87a577283443cc1fc1d4b9dfb7ee1d64449
Kirk Rinon <kirk.rinon@humaniti.com>
2019-12-15 11:15:09 +1100

    [modify] implemented MAT_DIALOG from angular material for modal component

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
+import {DialogData, ResultsComponent} from "../results/results.component";
 
 @Component({
   selector: 'app-modal',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<ResultsComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
 
   ngOnInit() {
+  }
+
+  onCloseClick(): void {
+    this.dialogRef.close();
   }
 
 }
