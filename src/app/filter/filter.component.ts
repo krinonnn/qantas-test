fd805f1b34fa02b88d635d62654c7ac511c9051e
Kirk Rinon <kirk.rinon@humaniti.com>
2019-12-15 11:13:53 +1100

    [modify] results component handling the response from app.service

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-filter',
@@ -7,16 +7,34 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent implements OnInit {
+  @Output() submitFilters: EventEmitter<any> = new EventEmitter();
   activityTypes = [
     'relaxation',
-    'recreation',
+    'recreational',
     'busywork',
     'music',
     'charity',
     'education'
   ];
 
-  constructor() {}
+  activityFilterForm: FormGroup;
 
-  ngOnInit() {}
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.activityFilterForm = this.formBuilder.group({
+      min: ['0'],
+      max: ['100'],
+      type: ['any']
+    });
+  }
+
+  submitFilterValues() {
+    const filtersObject = {
+      min: this.activityFilterForm.controls.min.value, max:
+      this.activityFilterForm.controls.max.value, type: this.activityFilterForm.controls.type.value
+    };
+
+    this.submitFilters.emit(filtersObject);
+  }
 }
