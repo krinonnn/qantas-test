845f65ec81eae799ab0da7d2b8b927014f8ee6b8
Kirk Rinon <kirk.rinon@humaniti.com>
2019-12-15 11:09:51 +1100

    [modify] implemented API connection via httpClient Module
    - 2 url options: with a type OR without type depending on filter params

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Activity} from "./shared/activity";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
-  constructor() {}
 
-  public getActivity(params: any): any {
-    // TODO: Make a request
+  baseURL = 'https://www.boredapi.com/api';
+
+  constructor(private http: HttpClient) {}
+
+  public getActivity(params): Observable<Activity> {
+    const min = params.min / 100;
+    const max = params.max / 100;
+    if (params.type !== 'any') {
+      return this.http.get<Activity>(this.baseURL + '/activity?type=' + params.type + '&minprice=' + min +
+      '&maxprice=' + max);
+    } else {
+      return this.http.get<Activity>(this.baseURL + '/activity?minprice=' + min +
+      '&maxprice=' + max);
+    }
   }
 }
